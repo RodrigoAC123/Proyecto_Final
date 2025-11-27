29b81cfb1caf43f65d7bbb491966374f9e5a1f81
RodrigoAC123 <rodrigoact3c@gmail.com>
2025-11-26 18:48:23 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ y = datos['Costo']
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=01613726)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1613726)
 LR = LinearRegression()
 LR.fit(X_train,y_train)
 
